29ae26e493fdfb552fcd5a1d01e72059408492e8
vishakhamulik <69720803+vishakhamulik@users.noreply.github.com>
2021-05-11 15:31:24 +0530

    Add files via upload

---
+++ cf.php
@@ -0,0 +1,18 @@
+<?php
+
+ 
+   $servername= "localhost";
+   $username="root";
+   $password="";
+   $dbname="db";
+   
+$conn = new mysqli ($servername, $username, $password, $dbname);
+
+if (!$conn) {
+  die("Connection failed: " . mysqli_connect_error());
+}
+
+error_reporting(E_ALL & ~E_NOTICE ^ E_DEPRECATED);
+
+
+?>
